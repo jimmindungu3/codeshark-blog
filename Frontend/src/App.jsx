3e96b7b2d6d64d68de5b0ca6d93941d8ee8cffd4
james <jimmindungu3@gmail.com>
2024-12-18 12:50:51 +0300

    config sidebar for mobile

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import Body from "./components/Body";
 
 function App() {
   return (
-    <div className="bg-gray-300 dark:bg-gray-950 dark:bg-opacity-95">
+    <div className="md:bg-gray-300 dark:bg-gray-950 dark:bg-opacity-95">
       <Navbar />
       <Body />
     </div>
--- Frontend/src/components/SideBar.jsx
+++ Frontend/src/components/SideBar.jsx
@@ -24,6 +24,40 @@ const SideBar = () => {
       </div>
       <div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
         <a href="#">How to Build a RESTful API with Node.js</a>
+      </div><div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">The Big Choice: First Programming Language</a>
+      </div>
+      <div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">Top 5 JavaScript Frameworks in 2025</a>
+      </div>
+      <div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">Understanding Asynchronous Programming</a>
+      </div>
+      <div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">Tailwind CSS vs. Bootstrap: Which to Choose?</a>
+      </div>
+      <div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">React vs. Vue: The Developer's Perspective</a>
+      </div>
+      <div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">How to Build a RESTful API with Node.js</a>
+      </div><div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">The Big Choice: First Programming Language</a>
+      </div>
+      <div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">Top 5 JavaScript Frameworks in 2025</a>
+      </div>
+      <div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">Understanding Asynchronous Programming</a>
+      </div>
+      <div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">Tailwind CSS vs. Bootstrap: Which to Choose?</a>
+      </div>
+      <div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">React vs. Vue: The Developer's Perspective</a>
+      </div>
+      <div className="text-blue-500 hover:underline dark:text-gray-300 font-semibold mt-6">
+        <a href="#">How to Build a RESTful API with Node.js</a>
       </div>
     </div>
   );
